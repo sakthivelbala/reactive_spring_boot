8f9273d6e6812eb8d04fd1ea8f76db6a666ee7dc
Sakthivel Balakrishnan <sakthivelbalakrishnan@gmail.com>
2020-11-29 19:32:46 +0530

    initial commit

---
+++ src/main/java/com/sakthidev/reactivespringboot/ReactiveSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sakthidev.reactivespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringBootApplication.class, args);
+	}
+
+}

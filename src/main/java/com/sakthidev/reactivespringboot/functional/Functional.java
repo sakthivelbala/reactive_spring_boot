53645daf309a4e6175e3ee70282c7b55cae2ea84
Sakthivel Balakrishnan <sakthivelbalakrishnan@gmail.com>
2020-11-29 20:24:22 +0530

    moved functional

--- src/main/java/com/sakthidev/reactivespringboot/functional/Functional.java
+++ src/main/java/com/sakthidev/reactivespringboot/functional/Functional.java
@@ -1,4 +1,4 @@
-package com.sakthidev.reactivespringboot;
+package com.sakthidev.reactivespringboot.functional;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;

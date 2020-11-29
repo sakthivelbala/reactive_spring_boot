eff502dcf08f97b9c3a1825ee66b4fcd7beb94c3
Sakthivel Balakrishnan <sakthivelbalakrishnan@gmail.com>
2020-11-29 20:18:33 +0530

    outline code

---
+++ src/main/java/com/sakthidev/reactivespringboot/Functional.java
@@ -0,0 +1,15 @@
+package com.sakthidev.reactivespringboot;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Functional implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.print("Functional");
+    }
+    
+}
